45ec941c4f1d6ae50147081f9c26d03dbed9298d
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 02:22:32 +0700

    Add some other commands

--- scripts/main.py
+++ scripts/main.py
@@ -2,9 +2,28 @@ from input import get_user_input
 from process_mood import get_wallpaper_path
 from change_wallpaper import change_wallpaper
 
+from mood_file import (
+    make_mood_file,
+    read_mood_file
+)
+
 # Get user input
 mood = get_user_input()
 
+# Different commands for different moods
+if mood == "ls":
+    
+    # Print all the moods current available
+    read_mood_file()
+    exit()
+    
+elif mood == "config":
+    
+    # Open the file for the user to edit
+    make_mood_file()
+    exit()
+
+# If not the above commands, continue the process
 # Process user mood
 wallpaper_path = get_wallpaper_path(mood)
 
