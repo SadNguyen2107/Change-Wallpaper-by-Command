4bfbf2922111f7bb8e46b5699a7daee628bc9296
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 01:04:19 +0700

    Process user mood in main

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,8 @@
 from input import get_user_input
+from process_mood import mood_to_wallpaper
 
 # Get user input
 mood = get_user_input()
+
+# Process user mood
+wallpaper_path = mood_to_wallpaper[mood]
