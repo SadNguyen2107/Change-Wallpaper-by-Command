cb7a2b01529d73f7036886432acea2df4aac5f0d
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 03:12:37 +0700

    Update some code

--- scripts/process_mood.py
+++ scripts/process_mood.py
@@ -9,7 +9,13 @@ def get_wallpaper_path(mood):
     """
 
     # Read the mood file
-    wallpaper_path_dict = read_mood_file()
+    config = read_mood_file()
+    wallpaper_path_dict = config["Wallpapers"]
+
+    # Check if mood exists
+    if mood not in wallpaper_path_dict:
+        print("Mood not found.")
+        exit()
 
     # Get wallpaper path
     wallpaper_path = wallpaper_path_dict.get(mood)
