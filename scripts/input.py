ac8ecc19de6d5610ab1f383b4fca6e6509f6a404
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 00:56:36 +0700

    Change input to mood

--- scripts/input.py
+++ scripts/input.py
@@ -9,7 +9,7 @@ def get_user_input():
     """
 
     # Get user input
-    parser = argparse.ArgumentParser(description="Process user input.")
+    parser = argparse.ArgumentParser(description="Process user mood.")
     parser.add_argument("mood", type=str, help="User mood")
     args = parser.parse_args()
 
