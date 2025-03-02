86cab9ea7c01bc10a1253b6ab2a4c83299b94934
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 02:26:14 +0700

    Fix some commands

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,11 @@ from input import get_user_input
 from process_mood import get_wallpaper_path
 from change_wallpaper import change_wallpaper
 
-from mood_file import make_mood_file, read_mood_file
+from mood_file import (
+    make_mood_file, 
+    read_mood_file,
+    edit_mood_file,
+)
 
 # Get user input
 mood = get_user_input()
@@ -17,7 +21,7 @@ if mood == "ls":
 elif mood == "config":
 
     # Open the file for the user to edit
-    make_mood_file()
+    edit_mood_file()
     exit()
 
 # If not the above commands, continue the process
