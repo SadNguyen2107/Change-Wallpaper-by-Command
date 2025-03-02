0b4bc07a600a5c8613fdfab328ce0da1aeee8702
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 01:00:47 +0700

    Add dict to process mood

---
+++ scripts/process_mood.py
@@ -0,0 +1,6 @@
+mood_to_wallpaper = {
+    "study": r"C:\Users\User\OneDrive - Hanoi University of Science and Technology\Pictures\Wallpapers\peakpx (1).jpg",
+    "game": r"C:\Users\User\OneDrive - Hanoi University of Science and Technology\Pictures\Wallpapers\valorant-pc-games-2022-games-5k-8k-neon-typography-dark-3840x2160-8532.jpg",
+    "waifu": r"C:\Users\User\OneDrive - Hanoi University of Science and Technology\Pictures\Wallpapers\miside-mita-dark-3840x2160-21051.jpg",
+}
+
