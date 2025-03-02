5eaad00829f7c571fac777a79eda51c74408a4f2
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 02:22:07 +0700

    Remove the dictionary from the beginning

--- scripts/process_mood.py
+++ scripts/process_mood.py
@@ -1,11 +1,6 @@
-mood_to_wallpaper = {
-    "study": r"C:\Users\User\OneDrive - Hanoi University of Science and Technology\Pictures\Wallpapers\peakpx (1).jpg",
-    "game": r"C:\Users\User\OneDrive - Hanoi University of Science and Technology\Pictures\Wallpapers\valorant-pc-games-2022-games-5k-8k-neon-typography-dark-3840x2160-8532.jpg",
-    "waifu": r"C:\Users\User\OneDrive - Hanoi University of Science and Technology\Pictures\Wallpapers\miside-mita-dark-3840x2160-21051.jpg",
-}
-
 from pathlib import Path
 import json
+
 
 def get_wallpaper_path(mood):
     """
@@ -19,7 +14,7 @@ def get_wallpaper_path(mood):
 
     # Create a file in the home directory
     file_path = home_dir / "mood.json"
-    
+
     # Open the mood file
     with open(str(file_path), "r") as file:
         mood_to_wallpaper = json.load(file)
