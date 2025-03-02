7b819a0a6bc140ab17913a872ae68666e4e68afe
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 03:13:01 +0700

    Update commands

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,7 @@ from change_wallpaper import change_wallpaper
 
 from mood_file import (
     is_mood_file_exist,
-    read_mood_file,
+    print_mood_file,
     edit_mood_file,
     make_mood_file,
 )
@@ -22,10 +22,10 @@ if not is_mood_file_exist():
 elif mood == "ls":
 
     # Print all the moods current available
-    read_mood_file()
+    print_mood_file()
     exit()
 
-elif mood == "config":
+elif mood == "edit":
 
     # Open the file for the user to edit
     edit_mood_file()
