ca744e570d19bc593e1da5853cda405029c9a843
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 01:33:22 +0700

    Add function get_wallpaper_path

--- scripts/process_mood.py
+++ scripts/process_mood.py
@@ -4,3 +4,27 @@ mood_to_wallpaper = {
     "waifu": r"C:\Users\User\OneDrive - Hanoi University of Science and Technology\Pictures\Wallpapers\miside-mita-dark-3840x2160-21051.jpg",
 }
 
+from pathlib import Path
+import json
+
+def get_wallpaper_path(mood):
+    """
+    Get wallpaper path from mood
+    :param mood: user mood
+    :return: wallpaper path
+    """
+
+    # Get the home directory
+    home_dir = Path.home()
+
+    # Create a file in the home directory
+    file_path = home_dir / "mood.json"
+    
+    # Open the mood file
+    with open(str(file_path), "r") as file:
+        mood_to_wallpaper = json.load(file)
+
+    # Get wallpaper path
+    wallpaper_path = mood_to_wallpaper[mood]
+
+    return wallpaper_path
