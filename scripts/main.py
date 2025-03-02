b30ea3426e1862455cf766eda0a8f1109c1f2c75
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 01:10:52 +0700

    Call change_wallpaper in main

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,12 @@
 from input import get_user_input
 from process_mood import mood_to_wallpaper
+from change_wallpaper import change_wallpaper
 
 # Get user input
 mood = get_user_input()
 
 # Process user mood
 wallpaper_path = mood_to_wallpaper[mood]
+
+# Change wallpaper
+change_wallpaper(wallpaper_path)
