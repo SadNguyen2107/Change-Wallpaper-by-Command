ed2619696764865effd5cfb7751c20b89ce99f02
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-03-03 03:01:16 +0700

    Replace JSON to INI file

--- scripts/mood_file.py
+++ scripts/mood_file.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import subprocess
-import json
+import configparser
 
 # Get the home directory
 home_dir = Path.home()
 
 # Create a file in the home directory
-file_path = home_dir / "mood.json"
+file_path = home_dir / "mood.ini"
 
 
 def is_mood_file_exist():
@@ -19,11 +19,13 @@ def is_mood_file_exist():
 
 def edit_mood_file():
     """
-    Open the mood.json file for the user to edit
+    Open the mood.ini file for the user to edit
     """
 
     # VS Code path
-    vs_code_path = home_dir / "AppData" / "Local" / "Programs" / "Microsoft VS Code" / "Code.exe"
+    vs_code_path = (
+        home_dir / "AppData" / "Local" / "Programs" / "Microsoft VS Code" / "Code.exe"
+    )
 
     # Open VS Code and wait for the user to close it before continuing
     subprocess.run(
@@ -37,19 +39,39 @@ def edit_mood_file():
 
 def make_mood_file():
     """
-    Create a mood.json in the home directory
+    Create a mood.ini file in the home directory
     """
 
-    # Create the file
+    config = configparser.ConfigParser()
+    config["Wallpapers"] = {"Your mood here": "wallpaper path here"}
+
     with open(file_path, "w") as file:
-        file.write('{\n\t"Your mood here": "wallpaper path here"\n}')
+        config.write(file)
 
     # Open the file for the user to edit
     edit_mood_file()
 
 
-def read_mood_file():
-    with open(str(file_path), "r") as file:
-        mood_to_wallpaper = json.load(file)
+def read_mood_file() -> dict["str"]:
+    """
+    Reads the mood.ini file and returns the stored moods.
 
-    print("\n".join(mood_to_wallpaper))
+    If the file does not exist, it creates one and prompts the user to edit it.
+    
+    :return: A dictionary containing mood-to-wallpaper mappings.
+    """
+    
+    if not is_mood_file_exist():
+        print("Mood file does not exist. Creating one now...")
+        make_mood_file()
+
+    config = configparser.ConfigParser()
+    config.read(file_path)
+
+    if "Wallpapers" in config:
+        moods = config["Wallpapers"]
+        print("\n".join(moods))
+    else:
+        print("No wallpapers section found!")
+
+    return config["Wallpapers"]
